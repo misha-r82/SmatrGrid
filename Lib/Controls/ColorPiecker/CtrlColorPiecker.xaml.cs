eac9740e959db7bff9e67cb794e0b02cb972925b
misha_r82@mail.ru <Mir232du>
2020-04-24 16:58:40 +0300

    hsl цвета

--- Lib/Controls/ColorPiecker/CtrlColorPiecker.xaml.cs
+++ Lib/Controls/ColorPiecker/CtrlColorPiecker.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,27 +26,10 @@ namespace Lib.Controls.ColorPiecker
         {
             InitializeComponent();
             InitialWork();
-            //image.Source = loadBitmap(DropDownCustomColorPicker.Properties.Resources.ColorSwatchCircle);
-            txtAlpha.LostFocus += new RoutedEventHandler(txtAlpha_TextChanged);
-            txtR.LostFocus += new RoutedEventHandler(txtR_TextChanged);
-            txtG.LostFocus += new RoutedEventHandler(txtG_TextChanged);
-            txtB.LostFocus += new RoutedEventHandler(txtB_TextChanged);
-            txtAll.LostFocus += new RoutedEventHandler(txtAll_TextChanged);
-            txtAlpha.KeyDown += new KeyEventHandler(txtAlpha_KeyDown);
-            txtR.KeyDown += new KeyEventHandler(txtR_KeyDown);
-            txtG.KeyDown += new KeyEventHandler(txtG_KeyDown);
-            txtB.KeyDown += new KeyEventHandler(txtB_KeyDown);
-            txtAll.KeyDown += new KeyEventHandler(txtAll_KeyDown);
-            CanColor.MouseLeftButtonDown += new MouseButtonEventHandler(CanColor_MouseLeftButtonDown);
-            CanColor.MouseLeftButtonUp += new MouseButtonEventHandler(CanColor_MouseLeftButtonUp);
-            EpPointer.MouseMove += new MouseEventHandler(EpPointer_MouseMove);
-            EpPointer.MouseLeftButtonDown += new MouseButtonEventHandler(EpPointer_MouseLeftButtonDown);
-            EpPointer.MouseLeftButtonUp += new MouseButtonEventHandler(EpPointer_MouseLeftButtonUp);
+
         }
 
-        private bool IsMouseDownOverEllipse = false;
         private bool _shift = false;
-
         private Color _customColor = Colors.Transparent;
 
         public Color CustomColor
@@ -56,309 +40,8 @@ namespace Lib.Controls.ColorPiecker
                 if (_customColor != value)
                 {
                     _customColor = value;
-                    UpdatePreview();
                 }
             }
-        }
-
-
-
-        void EpPointer_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            IsMouseDownOverEllipse = false;
-        }
-
-        void CanColor_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            e.Handled = true;
-        }
-
-        void CanColor_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            ChangeColor();
-            e.Handled = true;
-        }
-
-        void EpPointer_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            IsMouseDownOverEllipse = true;
-        }
-
-        void EpPointer_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (IsMouseDownOverEllipse)
-            {
-                ChangeColor();
-            }
-            e.Handled = true;
-        }
-
-        void txtAll_KeyDown(object sender, KeyEventArgs e)
-        {
-
-            if (e.Key == Key.Enter)
-            {
-                try
-                {
-                    if (string.IsNullOrEmpty(((TextBox)sender).Text)) return;
-                    CustomColor = MakeColorFromHex(sender);
-                    Reposition();
-                }
-                catch
-                {
-                }
-            }
-            else if (e.Key == Key.Tab)
-            {
-                txtAlpha.Focus();
-            }
-
-            string input = e.Key.ToString().Substring(1);
-            if (string.IsNullOrEmpty(input))
-            {
-                input = e.Key.ToString();
-            }
-            if (input == "3" && _shift == true)
-            {
-                input = "#";
-            }
-
-            if (e.Key == Key.LeftShift || e.Key == Key.RightShift)
-            {
-                _shift = true;
-            }
-            else
-            {
-                _shift = false;
-            }
-
-            if (!(input == "#" || (input[0] >= 'A' && input[0] <= 'F') || (input[0] >= 'a' && input[0] <= 'F') || (input[0] >= '0' && input[0] <= '9')))
-                e.Handled = true;
-            if (input.Length > 1)
-                e.Handled = true;
-        }
-
-        void txtAlpha_LostFocus(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        void txtB_KeyDown(object sender, KeyEventArgs e)
-        {
-            NumericValidation(e);
-            NumericValidation(e);
-            if (e.Key == Key.Tab)
-            {
-                txtAll.Focus();
-            }
-
-        }
-
-        void txtG_KeyDown(object sender, KeyEventArgs e)
-        {
-            NumericValidation(e);
-            if (e.Key == Key.Tab)
-            {
-                txtB.Focus();
-            }
-        }
-
-        void txtR_KeyDown(object sender, KeyEventArgs e)
-        {
-            NumericValidation(e);
-            if (e.Key == Key.Tab)
-            {
-                txtG.Focus();
-            }
-        }
-
-        void txtAlpha_KeyDown(object sender, KeyEventArgs e)
-        {
-            NumericValidation(e);
-
-            if (e.Key == Key.Tab)
-            {
-                txtR.Focus();
-            }
-        }
-
-        void txtAll_TextChanged(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                if (string.IsNullOrEmpty(((TextBox)sender).Text)) return;
-                CustomColor = MakeColorFromHex(sender);
-                Reposition();
-            }
-            catch
-            {
-            }
-
-        }
-
-        private Color MakeColorFromHex(object sender)
-        {
-            try
-            {
-                ColorConverter cc = new ColorConverter();
-                return (Color)cc.ConvertFrom(((TextBox)sender).Text);
-
-            }
-            catch
-            {
-                string alphaHex = CustomColor.A.ToString("X").PadLeft(2, '0');
-                string redHex = CustomColor.R.ToString("X").PadLeft(2, '0');
-                string greenHex = CustomColor.G.ToString("X").PadLeft(2, '0');
-                string blueHex = CustomColor.B.ToString("X").PadLeft(2, '0');
-                txtAll.Text = String.Format("#{0}{1}{2}{3}",
-                alphaHex, redHex,
-                greenHex, blueHex);
-
-
-            }
-            return _customColor;
-        }
-
-        void txtB_TextChanged(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                if (string.IsNullOrEmpty(((TextBox)sender).Text)) return;
-                int val = Convert.ToInt32(((TextBox)sender).Text);
-                if (val > 255)
-
-                    ((TextBox)sender).Text = "255";
-                else
-                {
-                    byte byteValue = Convert.ToByte(((TextBox)sender).Text);
-                    CustomColor = MakeColorFromRGB();
-                    Reposition();
-                }
-            }
-            catch
-            {
-            }
-
-        }
-
-        private Color MakeColorFromRGB()
-        {
-            byte abyteValue = Convert.ToByte(txtAlpha.Text);
-            byte rbyteValue = Convert.ToByte(txtR.Text);
-            byte gbyteValue = Convert.ToByte(txtG.Text);
-            byte bbyteValue = Convert.ToByte(txtB.Text);
-            Color rgbColor =
-                 Color.FromArgb(
-                     abyteValue,
-                     rbyteValue,
-                     gbyteValue,
-                     bbyteValue);
-            return rgbColor;
-        }
-
-        void txtG_TextChanged(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                if (string.IsNullOrEmpty(((TextBox)sender).Text)) return;
-                int val = Convert.ToInt32(((TextBox)sender).Text);
-                if (val > 255)
-
-                    ((TextBox)sender).Text = "255";
-                else
-                {
-                    byte byteValue = Convert.ToByte(((TextBox)sender).Text);
-                    CustomColor =
-                       Color.FromArgb(
-                            _customColor.A,
-                           CustomColor.R,
-                          byteValue,
-                           CustomColor.B);
-                    Reposition();
-
-                }
-            }
-            catch
-            {
-            }
-        }
-
-        void txtR_TextChanged(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-
-                if (string.IsNullOrEmpty(((TextBox)sender).Text)) return;
-                int val = Convert.ToInt32(((TextBox)sender).Text);
-                if (val > 255)
-
-                    ((TextBox)sender).Text = "255";
-                else
-                {
-                    byte byteValue = Convert.ToByte(((TextBox)sender).Text);
-                    CustomColor =
-                       Color.FromArgb(
-                            _customColor.A,
-                           byteValue,
-                           CustomColor.G,
-                           CustomColor.B);
-                    Reposition();
-
-                }
-            }
-            catch
-            {
-            }
-        }
-
-        void txtAlpha_TextChanged(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                if (string.IsNullOrEmpty(((TextBox)sender).Text)) return;
-                int val = Convert.ToInt32(((TextBox)sender).Text);
-                if (val > 255)
-
-                    ((TextBox)sender).Text = "255";
-                else
-                {
-                    byte byteValue = Convert.ToByte(((TextBox)sender).Text);
-                    CustomColor =
-                       Color.FromArgb(
-                            byteValue,
-                           CustomColor.R,
-                           CustomColor.G,
-                           CustomColor.B);
-
-                }
-            }
-            catch
-            {
-            }
-        }
-
-        private void NumericValidation(System.Windows.Input.KeyEventArgs e)
-        {
-            string input = e.Key.ToString().Substring(1);
-            try
-            {
-                if (e.Key == Key.Enter)
-                {
-                    CustomColor = MakeColorFromRGB();
-                    Reposition();
-                }
-                int inputDigit = Int32.Parse(input);
-            }
-            catch
-            {
-                e.Handled = true;
-            }
-        }
-
-        public static BitmapSource loadBitmap(System.Drawing.Bitmap source)
-        {
-            return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(source.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty,
-                System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
         }
 
         private void InitialWork()
@@ -394,118 +77,15 @@ namespace Lib.Controls.ColorPiecker
             }
         }
 
-        private void ChangeColor()
-        {
-            try
-            {
-                CustomColor = GetColorFromImage((int)Mouse.GetPosition(CanColor).X, (int)Mouse.GetPosition(CanColor).Y);
-                MovePointer();
-            }
-            catch
-            {
-
-            }
-        }
-
-        private void Reposition()
-        {
-
-            for (int i = 0; i < CanColor.ActualWidth; i++)
-            {
-                bool flag = false;
-                for (int j = 0; j < CanColor.ActualHeight; j++)
-                {
-
-                    try
-                    {
-                        Color Colorfromimagepoint = GetColorFromImage(i, j);
-                        if (SimmilarColor(Colorfromimagepoint, _customColor))
-                        {
-                            MovePointerDuringReposition(i, j);
-                            flag = true;
-                            break;
-                        }
-
-                    }
-                    catch
-                    {
-
-                    }
-
-                }
-                if (flag) break;
-
-            }
-
-
-        }
-        /// <summary>
-        /// 1*1 pixel copy is based on an article by Lee Brimelow    
-        /// http://thewpfblog.com/?p=62
-        /// </summary>
-
-        private Color GetColorFromImage(int i, int j)
-        {
-            CroppedBitmap cb = new CroppedBitmap(image.Source as BitmapSource,
-                new Int32Rect(i,
-                    j, 1, 1));
-            byte[] color = new byte[4];
-            cb.CopyPixels(color, 4, 0);
-            Color Colorfromimagepoint = Color.FromArgb((byte)SdA.Value, color[2], color[1], color[0]);
-            return Colorfromimagepoint;
-        }
-
-        private void MovePointerDuringReposition(int i, int j)
-        {
-            EpPointer.SetValue(Canvas.LeftProperty, (double)(i - 3));
-            EpPointer.SetValue(Canvas.TopProperty, (double)(j - 3));
-            EpPointer.InvalidateVisual();
-            CanColor.InvalidateVisual();
-        }
-        private void MovePointer()
-        {
-            EpPointer.SetValue(Canvas.LeftProperty, (double)(Mouse.GetPosition(CanColor).X - 5));
-            EpPointer.SetValue(Canvas.TopProperty, (double)(Mouse.GetPosition(CanColor).Y - 5));
-            CanColor.InvalidateVisual();
-        }
-
-        private bool SimmilarColor(Color pointColor, Color selectedColor)
-        {
-            int diff = Math.Abs(pointColor.R - selectedColor.R) + Math.Abs(pointColor.G - selectedColor.G) + Math.Abs(pointColor.B - selectedColor.B);
-            if (diff < 20) return true;
-            else
-                return false;
-        }
-
-        private void UpdatePreview()
-        {
-            lblPreview.Background = new SolidColorBrush(CustomColor);
-            txtAlpha.Text = CustomColor.A.ToString();
-            string alphaHex = CustomColor.A.ToString("X").PadLeft(2, '0');
-            txtR.Text = CustomColor.R.ToString();
-            string redHex = CustomColor.R.ToString("X").PadLeft(2, '0');
-            txtG.Text = CustomColor.G.ToString();
-            string greenHex = CustomColor.G.ToString("X").PadLeft(2, '0');
-            txtB.Text = CustomColor.B.ToString();
-            string blueHex = CustomColor.B.ToString("X").PadLeft(2, '0');
-            txtAll.Text = String.Format("#{0}{1}{2}{3}",
-            alphaHex, redHex,
-            greenHex, blueHex);
-            SdA.Value = CustomColor.A;
-        }
-
         private void TabItem_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             e.Handled = true;
         }
 
-        private void SdA_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            CustomColor = Color.FromArgb((byte)SdA.Value, CustomColor.R, CustomColor.G, CustomColor.B);
-        }
+
             }
 
-            class CustomColors
+        class CustomColors
         {
             List<Color> _SelectableColors = null;
 
@@ -515,17 +95,140 @@ namespace Lib.Controls.ColorPiecker
                 set { _SelectableColors = value; }
             }
 
-            public CustomColors()
+
+        /// <summary>
+        /// Convert HSV to RGB
+        /// h is from 0-360
+        /// s,v values are 0-1
+        /// r,g,b values are 0-255
+        /// Based upon http://ilab.usc.edu/wiki/index.php/HSV_And_H2SV_Color_Space#HSV_Transformation_C_.2F_C.2B.2B_Code_2
+        /// </summary>
+        private Color HsvToRgb(double h, double S, double V)
+        {
+            // ######################################################################
+            // T. Nathan Mundhenk
+            // mundhenk@usc.edu
+            // C/C++ Macro HSV to RGB
+
+            double H = h;
+            while (H < 0) { H += 360; };
+            while (H >= 360) { H -= 360; };
+            double R, G, B;
+            if (V <= 0)
+            { R = G = B = 0; }
+            else if (S <= 0)
+            {
+                R = G = B = V;
+            }
+            else
+            {
+                double hf = H / 60.0;
+                int i = (int)Math.Floor(hf);
+                double f = hf - i;
+                double pv = V * (1 - S);
+                double qv = V * (1 - S * f);
+                double tv = V * (1 - S * (1 - f));
+                switch (i)
+                {
+
+                    // Red is the dominant color
+
+                    case 0:
+                        R = V;
+                        G = tv;
+                        B = pv;
+                        break;
+
+                    // Green is the dominant color
+
+                    case 1:
+                        R = qv;
+                        G = V;
+                        B = pv;
+                        break;
+                    case 2:
+                        R = pv;
+                        G = V;
+                        B = tv;
+                        break;
+
+                    // Blue is the dominant color
+
+                    case 3:
+                        R = pv;
+                        G = qv;
+                        B = V;
+                        break;
+                    case 4:
+                        R = tv;
+                        G = pv;
+                        B = V;
+                        break;
+
+                    // Red is the dominant color
+
+                    case 5:
+                        R = V;
+                        G = pv;
+                        B = qv;
+                        break;
+
+                    // Just in case we overshoot on our math by a little, we put these here. Since its a switch it won't slow us down at all to put these here.
+
+                    case 6:
+                        R = V;
+                        G = tv;
+                        B = pv;
+                        break;
+                    case -1:
+                        R = V;
+                        G = pv;
+                        B = qv;
+                        break;
+
+                    // The color is not defined, we should throw an error.
+
+                    default:
+                        //LFATAL("i Value error in Pixel conversion, Value is %d", i);
+                        R = G = B = V; // Just pretend its black/white
+                        break;
+                }
+            }
+            byte r = (byte)Clamp((int)(R * 255.0));
+            byte g = (byte)Clamp((int)(G * 255.0));
+            byte b = (byte)Clamp((int)(B * 255.0));
+            return Color.FromRgb(r,g,b);
+        }
+
+        /// <summary>
+        /// Clamp a value to 0-255
+        /// </summary>
+        int Clamp(int i)
+        {
+            if (i < 0) return 0;
+            if (i > 255) return 255;
+            return i;
+        }
+
+
+
+
+        public CustomColors()
             {
                 _SelectableColors = new List<Color>();
-                Type ColorsType = typeof(Colors);
-                PropertyInfo[] ColorsProperty = ColorsType.GetProperties();
+                byte step = 20;
+                byte down = 0;
+                int up = 360;
+                for (double h = 0; h < up; h+=step)
+                for (double l = 0.6; l < 1; l += 0.2)
+                for (double s = 0.6; s < 1; s+=0.2)
 
-                foreach (PropertyInfo property in ColorsProperty)
                 {
-                    _SelectableColors.Add((Color)ColorConverter.ConvertFromString(property.Name));
 
+                    
+                    _SelectableColors.Add(HsvToRgb(h,s,l));
                 }
+
             }
 
         }
