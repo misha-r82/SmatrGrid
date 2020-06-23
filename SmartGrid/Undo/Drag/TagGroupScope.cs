f6408fa1c368a32d5af837208bfc41af7b02d083
misha_r82 <misha_r82@mail.ru>
2020-06-23 08:37:54 +0300

    дома

---
+++ SmartGrid/Undo/Drag/TagGroupScope.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGrid.Undo.Drag
+{
+    public class TagGroupScope : UndoScope
+    {
+
+        public TagGroupScope(TagGroup grp)
+        {
+
+        }
+        public override void Undo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Rendo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool HasChanges { get; }
+    }
+}
